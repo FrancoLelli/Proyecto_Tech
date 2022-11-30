f2a5de055d21c8a51f7f440f07a21f56b178eb51
FrancoLelli <franlelli1966@gmail.com>
2022-11-30 19:43:39 -0300

    commit

--- js/js.js
+++ js/js.js
@@ -10,3 +10,15 @@ document.querySelectorAll('.nav-menu').forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active")
     /* navMenu.classList.remove("active") */
 }))
+
+
+anio = new Date().getFullYear()
+
+if(anio == "2023"){
+
+    document.getElementById("year").innerHTML = "2022 / 2023"
+}else{
+    document.getElementById("year").innerHTML = "2022"
+}
+
+
