faee4d34c949e31083575add79e0784610a1ad48
FrancoLelli <franlelli1966@gmail.com>
2022-12-01 09:31:51 -0300

    commit

--- js/js.js
+++ js/js.js
@@ -16,9 +16,8 @@ anio = new Date().getFullYear()
 
 if(anio == "2023"){
 
-    document.getElementById("year").innerHTML = "2022 / 2023"
+    document.getElementById("year").innerHTML = "- 2022 / 2023 ©"
 }else{
-    document.getElementById("year").innerHTML = "2022"
+    document.getElementById("year").innerHTML = "- 2022 ©"
 }
-
 
